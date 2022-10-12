dca15b04221405dc839f1b8e63e4248df6eb6205
Vivek Singh <petmongrels@gmail.com>
2022-10-12 14:20:17 +0530

    origin certificate in keystore

--- makefiles/ssl.mk
+++ makefiles/ssl.mk
@@ -1,3 +1,12 @@
 setup-ssl:
+#make directory
 	-mkdir server/src/main/resources/keystore
-	keytool -genkeypair -alias consult -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore server/src/main/resources/keystore/consult.p12 -validity 3650
+#remove
+	rm server/src/main/resources/keystore/consult.p12
+#Convert the certificate and private key to PKCS 12
+	openssl pkcs12 -export -in ~/Downloads/origin.cer -inkey ~/Downloads/private_key -name server.yyyz.link -out origin-PKCS-12.p12
+#Import the PKCS 12 certificate by executing the following command
+	keytool -importkeystore -deststorepass password -destkeystore server/src/main/resources/keystore/consult.p12 -srckeystore origin-PKCS-12.p12 -srcstoretype PKCS12
+
+open-keystore-dir:
+	open server/src/main/resources/keystore
