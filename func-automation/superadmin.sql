21c83582d883cd42fd062969a9f576e03c4a8836
Vivek Singh <petmongrels@gmail.com>
2022-10-31 11:46:21 +0530

    run both http and https port

---
+++ func-automation/superadmin.sql
@@ -0,0 +1,2 @@
+update users set password = '$2a$10$ZAFNB4gI9rn011mUJq4DB.5qOHKfHD6R85ktW0jpDMwe08iSkkLIO' where
+    email = 'Super_Admin@example.com' and name = 'Super Admin' and password = 'Password not setup';
